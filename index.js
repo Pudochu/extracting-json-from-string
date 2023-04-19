99513cb82453e2e80728aa4958361fb369fc218e
Volkan <42183076+Pudochu@users.noreply.github.com>
2023-04-19 18:21:45 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+async function extractJSON(str) {
+    return await new Promise((resolve) => {
+        let data = str;
+        try {
+            if (typeof data === 'string') {
+                data = JSON.parse(str);
+            }
+        } catch (e) {
+            try {
+                const jsonStart = str.indexOf("{");
+                const jsonEnd = str.lastIndexOf("}");
+                const jsonString = str.slice(jsonStart, jsonEnd + 1);
+                resolve(JSON.parse(jsonString));
+            } catch (e) {
+                console.log(e)
+                resolve(false)
+            }
+        }
+        resolve(data);
+    });
+}
+
+extractJSON('demo string {"text": "hi!"}'); // result: {"text": "hi!"}
+extractJSON({"text": "hi!"}); // result: {"text": "hi!"}
+extractJSON("hello"); // result: false;
